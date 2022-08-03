8ebe880da2d7a82ba7d8f4f989d62afff5b62e48
hyewonShin <67300338+hyewonShin@users.noreply.github.com>
2022-08-03 22:37:03 +0900

    express 미들웨어 이해하기

--- dist/app.js
+++ dist/app.js
@@ -3,9 +3,27 @@ Object.defineProperty(exports, "__esModule", { value: true });
 var express = require("express");
 var app_models_1 = require("./app.models");
 var app = express();
+app.use(function (req, res, next) {
+    console.log(req.rawHeaders[1]);
+    console.log("this is middlware");
+    next();
+});
+app.get("/cats/som", function (req, res, next) {
+    console.log("이것은 som 미들웨어이다!");
+    next();
+});
 app.get("/", function (req, res) {
-    console.log(req);
     res.send({ cats: app_models_1.Cat });
+});
+app.get("/cats/blue", function (req, res, next) {
+    res.send({ blue: app_models_1.Cat[0] });
+});
+app.get("/cats/som", function (req, res) {
+    res.send({ som: app_models_1.Cat[1] });
+});
+app.use(function (req, res, next) {
+    console.log("this is error middleware");
+    res.send({ error: "404에러입니다." });
 });
 app.listen(8000, function () {
     console.log("server is on...");
--- src/app.ts
+++ src/app.ts
@@ -3,11 +3,37 @@ import { Cat, CatType } from "./app.models";
 
 const app: express.Express = express();
 
+app.use((req, res, next) => {
+  console.log(req.rawHeaders[1]);
+  console.log("this is middlware");
+  next();
+});
+
+app.get("/cats/som", (req, res, next) => {
+  console.log("이것은 som 미들웨어이다!");
+  next();
+});
+
 app.get("/", (req: express.Request, res: express.Response) => {
-  console.log(req);
   res.send({ cats: Cat });
+});
+
+app.get("/cats/blue", (req, res, next: express.NextFunction) => {
+  res.send({ blue: Cat[0] });
+});
+
+app.get("/cats/som", (req, res) => {
+  res.send({ som: Cat[1] });
+});
+
+app.use((req, res, next) => {
+  console.log("this is error middleware");
+  res.send({ error: "404에러입니다." });
 });
 
 app.listen(8000, () => {
   console.log("server is on...");
 });
+
+// app.use : 모든 api에 미들웨어로 사용 가능
+// app.get : 특정 api에 미들웨어로 사용 가능
