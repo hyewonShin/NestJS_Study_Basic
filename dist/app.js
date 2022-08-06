92052ecb87391012024346b339edcb9921efbe1a
hyewonShin <67300338+hyewonShin@users.noreply.github.com>
2022-08-06 17:04:50 +0900

    고양이 데이터 Creat Read API 개발

--- dist/app.js
+++ dist/app.js
@@ -8,24 +8,53 @@ app.use(function (req, res, next) {
     console.log("this is middlware");
     next();
 });
-app.get("/cats/som", function (req, res, next) {
-    console.log("이것은 som 미들웨어이다!");
-    next();
+app.use(express.json());
+app.get("/cats", function (req, res) {
+    try {
+        var cats = app_models_1.Cat;
+        res.status(200).send({
+            success: true,
+            data: {
+                cats: cats,
+            },
+        });
+    }
+    catch (error) {
+        res.status(400).send({
+            success: false,
+            error: error.massage,
+        });
+    }
 });
-app.get("/", function (req, res) {
-    res.send({ cats: app_models_1.Cat });
+app.get("/cat/:id", function (req, res) {
+    try {
+        var id_1 = req.params.id;
+        console.log("파라미터id확인", id_1);
+        var cat = app_models_1.Cat.find(function (cats) {
+            return cats.id === id_1;
+        });
+        res.send({ success: true, data: cat });
+    }
+    catch (error) {
+        res.send({ success: false, error: error.message });
+    }
 });
-app.get("/cats/blue", function (req, res, next) {
-    res.send({ blue: app_models_1.Cat[0] });
-});
-app.get("/cats/som", function (req, res) {
-    res.send({ som: app_models_1.Cat[1] });
+app.post("/cat", function (req, res) {
+    try {
+        var data = req.body;
+        console.log("Data", data);
+        app_models_1.Cat.push(data);
+        res.send({ success: true, data: { data: data } });
+    }
+    catch (error) {
+        res.send({ success: false, error: error.message });
+    }
 });
 app.use(function (req, res, next) {
     console.log("this is error middleware");
     res.send({ error: "404에러입니다." });
 });
-app.listen(8000, function () {
+app.listen(7000, function () {
     console.log("server is on...");
 });
 //# sourceMappingURL=app.js.map
--- src/app.ts
+++ src/app.ts
@@ -3,35 +3,69 @@ import { Cat, CatType } from "./app.models";
 
 const app: express.Express = express();
 
+// * logging meddleware
 app.use((req, res, next) => {
   console.log(req.rawHeaders[1]);
   console.log("this is middlware");
   next();
 });
 
-app.get("/cats/som", (req, res, next) => {
-  console.log("이것은 som 미들웨어이다!");
-  next();
+// * json middleware
+app.use(express.json());
+
+// * READ 고양이 전체 데이터 다 조회
+app.get("/cats", (req, res) => {
+  try {
+    const cats = Cat;
+    //throw new Error("에러발생!!");
+    res.status(200).send({
+      success: true,
+      data: {
+        cats,
+      },
+    });
+  } catch (error: any) {
+    res.status(400).send({
+      success: false,
+      error: error.massage,
+    });
+  }
 });
 
-app.get("/", (req: express.Request, res: express.Response) => {
-  res.send({ cats: Cat });
+// * READ 특정 고양이 데이터 조회
+app.get("/cat/:id", (req, res) => {
+  try {
+    //throw new Error("에러발생!!!");
+    const { id } = req.params;
+    console.log("파라미터id확인", id);
+    const cat = Cat.find((cats) => {
+      return cats.id === id;
+    });
+    res.send({ success: true, data: cat });
+  } catch (error: any) {
+    res.send({ success: false, error: error.message });
+  }
 });
 
-app.get("/cats/blue", (req, res, next: express.NextFunction) => {
-  res.send({ blue: Cat[0] });
+// * CREAT 새로운 고양이 추가 api
+app.post("/cat", (req, res) => {
+  try {
+    const data = req.body;
+    console.log("Data", data);
+    Cat.push(data); //creat
+    res.send({ success: true, data: { data } });
+  } catch (error: any) {
+    res.send({ success: false, error: error.message });
+  }
 });
 
-app.get("/cats/som", (req, res) => {
-  res.send({ som: Cat[1] });
-});
-
+// * 404 middleware
 app.use((req, res, next) => {
   console.log("this is error middleware");
   res.send({ error: "404에러입니다." });
 });
 
-app.listen(8000, () => {
+app.listen(7000, () => {
   console.log("server is on...");
 });
 
